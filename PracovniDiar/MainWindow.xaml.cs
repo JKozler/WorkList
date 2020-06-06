671d4aa13b54406585d9c2ce69aa3fd9e9d57f07
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2020-06-06 20:16:31 +0200

    Add project files.

---
+++ PracovniDiar/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace PracovniDiar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (File.Exists("Workers.txt"))
+            {
+                using (StreamReader sr = new StreamReader("workers.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lblWorkers.Items.Add(line);
+                    }
+                }
+            }
+        }
+
+        private void SelectedChangedLbl(object sender, SelectionChangedEventArgs e)
+        {
+            infoAboutUsers.Visibility = Visibility.Visible;
+            nameOfWorker.Content = lblWorkers.SelectedItem.ToString();
+        }
+    }
+}
