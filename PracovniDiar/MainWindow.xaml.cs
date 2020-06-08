fb06b8e07e91a98f0cb6c389bf31086965c4931b
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2020-06-08 13:29:25 +0200

    Tahání z dtabáze

--- PracovniDiar/MainWindow.xaml.cs
+++ PracovniDiar/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.IO.Packaging;
 
 namespace PracovniDiar
 {
@@ -24,6 +27,20 @@ namespace PracovniDiar
         public MainWindow()
         {
             InitializeComponent();
+
+            string connectionString = "SERVER=localhost;DATABASE=users;UID=root;PASSWORD=;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand("select * from infouser", connection);
+
+            connection.Open();
+
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+
+            connection.Close();
+
+            data.DataContext = dt;
+
             if (File.Exists("Workers.txt"))
             {
                 using (StreamReader sr = new StreamReader("workers.txt"))
@@ -39,8 +56,36 @@ namespace PracovniDiar
 
         private void SelectedChangedLbl(object sender, SelectionChangedEventArgs e)
         {
+            
+        }
+
+        private void removeUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lblWorkers.SelectedItem != null)
+            {
+                nameOfWorker.Content = "";
+                lblWorkers.Items.Remove(lblWorkers.SelectedItem);
+            }
+            else
+                MessageBox.Show("Musíte někoho vybrat.", "Chyba", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            infoAboutUsers.Visibility = Visibility.Hidden;
+        }
+
+        private void LblMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
             infoAboutUsers.Visibility = Visibility.Visible;
             nameOfWorker.Content = lblWorkers.SelectedItem.ToString();
+        }
+
+        private void ChBox1(object sender, RoutedEventArgs e)
+        {
+            workNotDone.IsChecked = false;
+        }
+
+        private void ChBox2(object sender, RoutedEventArgs e)
+        {
+            workDone.IsChecked = false;
         }
     }
 }
